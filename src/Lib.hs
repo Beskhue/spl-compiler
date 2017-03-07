be3e694ef882e0b4fbe8679697daee2f19543efb
Beskhue <thomas@kepow.org>
2017-03-07 06:15:41 +0100

    Add parse test function.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Parsec.Prim
+import Data.Token
+import qualified Lexer.Lexer as Lexer
+import qualified Parser.SPLParser as SPLParser
+
+compile :: String -> IO ()
+compile str = putStrLn "someFunc"
+
+parseTest  :: Show a => Parsec [TokenP] () a -> String -> IO ()
+parseTest p s =
+    case Lexer.lex "test" s of
+        Left e    -> putStrLn $ show e
+        Right ts -> SPLParser.parseTest p ts
