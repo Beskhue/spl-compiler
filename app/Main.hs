2c892291a548e00b2fc46ed6e1c1e95a99cbbf3c
Beskhue <thomas@kepow.org>
2017-03-07 07:19:54 +0100

    Add some convenience functions to Lib. Remove the now-redundant code from Main.

--- app/Main.hs
+++ app/Main.hs
@@ -5,20 +5,7 @@ import System.Environment
 import Data.NFA as NFA
 import qualified Text.Regex.Thompson as Regex
 import qualified Lexer.Lexer as Lexer
-import qualified Parser.Parser as Parser
+import qualified Parser.SPLParser as SPLParser
 
 main :: IO ()
-main = do
-    filePath <- getFilePath
-    rawSPL <- readFile filePath
-    putStrLn $ "Read '" ++ filePath ++ "'."
-    putStrLn rawSPL
-
-getFilePath :: IO String
-getFilePath = do
-    args <- getArgs
-    if length args > 0
-        then return $ head args
-        else do
-            putStr "Please provide a .spl program: "
-            getLine
+main = compile
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,28 @@
 module Lib where
 
+import System.Environment
 import Text.Parsec.Prim
 import Data.Token
 import qualified Lexer.Lexer as Lexer
 import qualified Parser.SPLParser as SPLParser
 
-compile :: String -> IO ()
-compile str = putStrLn "someFunc"
+getFilePath :: IO String
+getFilePath = do
+    args <- getArgs
+    if length args > 0
+        then return $ head args
+        else do
+            putStr "Please provide a .spl program: "
+            getLine
+
+compile :: IO ()
+compile = do
+    filePath <- getFilePath
+    rawSPL <- readFile filePath
+    putStrLn $ "Read '" ++ filePath ++ "'."
+    case Lexer.lex filePath rawSPL of
+        Left e    -> putStrLn $ show e
+        Right ts -> SPLParser.parseTest SPLParser.pSPL ts
 
 parseTest  :: Show a => Parsec [TokenP] () a -> String -> IO ()
 parseTest p s =
