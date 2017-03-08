dcc3f35ff8b8554a8eb6f54e424e796a84cbee13
Beskhue <thomas@kepow.org>
2017-03-08 20:57:58 +0100

    Add lexer tests.

--- test/Lexer/LexerTest.hs
+++ test/Lexer/LexerTest.hs
@@ -9,7 +9,50 @@ import Data.Pos
 import Lexer.Lexer as Lexer
 
 spec_Lexer :: Spec
-spec_Lexer = do
+spec_Lexer =
     describe "Lexer.lex" $ do
-        it "gives only TEOF for an empty string" $ do
+        it "gives only TEOF for an empty string" $
             getTokens (Lexer.lexDet "test" "") `shouldBe`  [TEOF]
+        it "gives only TEOF for code within a comment" $
+            getTokens (Lexer.lexDet "test" "/* Int a = 5; */") `shouldBe` [TEOF]
+        it "gives only TEOF for whitespace" $
+            getTokens (Lexer.lexDet "test" " \t\t\n\n\f\f  ") `shouldBe` [TEOF]
+        it "lexes open code that is between two block comments" $
+            getTokens (Lexer.lexDet "test" "/* Comment1 */ a /* Comment2 */") `shouldBe` [TIdentifier "a", TEOF]
+        it "lexes keywords as keywords and identifiers as identifiers" $
+            getTokens (Lexer.lexDet "test" "var if else while return vaa iff eelse retur") `shouldBe` [
+                TKeyword KVar, TKeyword KIf, TKeyword KElse, TKeyword KWhile, TKeyword KReturn,
+                TIdentifier "vaa", TIdentifier "iff", TIdentifier "eelse", TIdentifier "retur",
+                TEOF]
+        it "lexes field identifiers as fields" $
+            getTokens (Lexer.lexDet "test" "a.hd.fst.snd.tl") `shouldBe` [
+                TIdentifier "a",
+                TField FHd, TField FFst, TField FSnd, TField FTl,
+                TEOF]
+        it "lexes numbers as integer constants" $
+            getTokens (Lexer.lexDet "test" "1234567890") `shouldBe` [TConstant (CInt 1234567890), TEOF]
+        it "lexes hexadecimal numbers as integer constants" $
+            getTokens (Lexer.lexDet "test" "0xFF") `shouldBe` [TConstant (CInt 255), TEOF]
+        it "lexes True/False as boolean constants" $
+            getTokens (Lexer.lexDet "test" "True False") `shouldBe` [
+                TConstant (CBool True), TConstant (CBool False),
+                TEOF]
+        it "lexes characters as char constants" $
+            getTokens (Lexer.lexDet "test" "'a' 'ь' '0'") `shouldBe` [
+                TConstant (CChar 'a'), TConstant (CChar 'ь'), TConstant (CChar '0'), TEOF]
+        it "lexes non-ASCII characters as char constants" $
+            getTokens (Lexer.lexDet "test" "'ь'") `shouldBe` [TConstant (CChar 'ь'), TEOF]
+        it "lexes characters with special meaning in the syntax as char constants" $
+            getTokens (Lexer.lexDet "test" "'+' '!' ';'") `shouldBe` [
+                TConstant (CChar '+'), TConstant (CChar '!'), TConstant (CChar ';'), TEOF]
+        it "lexes an apostrophe as a character if and only if it is escaped" $ do
+            getTokens (Lexer.lexDet "test" "'''") `shouldBe` []
+            getTokens (Lexer.lexDet "test" "'\\''") `shouldBe` [TConstant (CChar '\''), TEOF]
+        it "keeps track of source position" $
+            Lexer.lexDet "test" "a\n\na a a/*\n\naaa*/a" `shouldBe` [
+                TP (TIdentifier "a") (Pos "test" 1 1),
+                TP (TIdentifier "a") (Pos "test" 3 1),
+                TP (TIdentifier "a") (Pos "test" 3 3),
+                TP (TIdentifier "a") (Pos "test" 3 5),
+                TP (TIdentifier "a") (Pos "test" 5 6),
+                TP TEOF (Pos "test" 5 7)]
