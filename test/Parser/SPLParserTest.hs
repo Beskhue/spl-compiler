ce0f841c1b5445aef040aad82617f9f736b160a7
Beskhue <thomas@kepow.org>
2017-03-16 20:58:11 +0100

    Add unicode example program.

--- test/Parser/SPLParserTest.hs
+++ test/Parser/SPLParserTest.hs
@@ -44,6 +44,7 @@ spec_SPLParser =
             parseDet rawSPL `shouldSatisfy` astEq (parseDet (prettyPrint (parseDet rawSPL)))
             rawSPL <- readFile "example-programs/ugly_style.spl"
             parseDet rawSPL `shouldSatisfy` astEq (parseDet (prettyPrint (parseDet rawSPL)))
-
+            rawSPL <- readFile "example-programs/unicode.spl"
+            parseDet rawSPL `shouldSatisfy` astEq (parseDet (prettyPrint (parseDet rawSPL)))
             --rawSPL <- readFile "example-programs/performance_test_parentheses.spl"
             --parseDet rawSPL `shouldSatisfy` astEq (parseDet (prettyPrint (parseDet rawSPL)))
