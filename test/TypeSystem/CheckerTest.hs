ef1966ddd729f9e8350608667331f48d4f54daaa
Beskhue <thomas@kepow.org>
2017-04-12 01:31:23 +0200

    Update type inference tests.

--- test/TypeSystem/CheckerTest.hs
+++ test/TypeSystem/CheckerTest.hs
@@ -2,9 +2,6 @@ module TypeSystem.CheckerTest where
 
 import Data.Either
 import Test.Tasty.Hspec as HSpec
-import qualified Data.Pos as Pos
-import qualified Data.AST as AST
-import Data.Pos
 import qualified Parser.SPLParser as SPLParser
 import qualified Lexer.Lexer as Lexer
 import TypeSystem.Checker as Checker
@@ -16,28 +13,24 @@ onlyTypeEmptyTInfExpr = Checker.onlyType . emptyTInfExpr
 spec_Checker :: Spec
 spec_Checker =
     describe "Checker.tInfExpr" $ do
-        it "infers that an int expression is of type int" $
-            let p =  Pos { sourceName = "test", line = 1, column = 1 } in
-                let a = (AST.ExprConstant (AST.ConstInt 5, p), p) in
-                    onlyTypeEmptyTInfExpr a `shouldBe` Right Checker.TInt
         it "infers that '1;'  is of type TInt" $
             onlyTypeEmptyTInfExpr (expr  "1;") `shouldBe` Right Checker.TInt
-        it "infers that '1 + 2' is of type TInt" $
+        it "infers that '1 + 2;' is of type TInt" $
             onlyTypeEmptyTInfExpr (expr  "1 + 2;") `shouldBe` Right Checker.TInt
-        it "infers that '(1 : []) : []' is of type [[TInt]]" $
+        it "infers that '(1 : []) : [];' is of type [[TInt]]" $
             onlyTypeEmptyTInfExpr (expr "(1 : []) : [];") `shouldBe` Right (Checker.TList (Checker.TList Checker.TInt))
-        it "infers that 'False : True : []' is of type [TBool]" $
+        it "infers that 'False : True : [];' is of type [TBool]" $
             onlyTypeEmptyTInfExpr (expr "True : False : [];") `shouldBe` Right (Checker.TList TBool)
-        it "infers that '[]' is a list of variable type" $
+        it "infers that '[];' is a list of variable type" $
             onlyTypeEmptyTInfExpr (expr "[];") `shouldSatisfy` (\t ->
                 case t of
                     Right (Checker.TList (Checker.TVar _)) -> True
                     _ -> False)
-        it "produces an error for '1 : 'a' : []'" $
+        it "produces an error for '1 : 'a' : [];'" $
             onlyTypeEmptyTInfExpr (expr "1 : 'a' : [];") `shouldSatisfy` isLeft
-        it "produces an error for '(1 : []) : (True : []) : []'" $
+        it "produces an error for '(1 : []) : (True : []) : [];'" $
             onlyTypeEmptyTInfExpr (expr "(1 : []) : (True : []) : [];") `shouldSatisfy` isLeft
-        it "produces an error for ''a' + 'b''" $
+        it "produces an error for ''a' + 'b';'" $
             onlyTypeEmptyTInfExpr (expr  "'a' + 'b';") `shouldSatisfy` isLeft
 
 
