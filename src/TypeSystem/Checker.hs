4428790707925ff4313c7b5acfe8c8776458740e
Beskhue <thomas@kepow.org>
2017-05-09 02:17:52 +0200

    Fix type of built-in function 'print'.

--- src/TypeSystem/Checker.hs
+++ src/TypeSystem/Checker.hs
@@ -162,7 +162,7 @@ emptyCtx = TypeCtx emptyMap
 
 builtInCtx :: TypeCtx
 builtInCtx = TypeCtx $ Map.fromList [
-    ("print", let t = TVar "a" in Scheme ["a"] (TFunction [t, t] TVoid)),
+    ("print", let t = TVar "a" in Scheme ["a"] (TFunction [t] TVoid)),
     ("isEmpty", let t = TList (TVar "a") in Scheme ["a"] (TFunction [t] TBool)),
     ("length", let t = TList (TVar "a") in Scheme ["a"] (TFunction [t] TInt))
     ]
